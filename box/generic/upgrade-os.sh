d07f02bff6008a79c26d277ae8612642c9d2eb20
Jerry Vonau <jvonau3@gmail.com>
2022-04-18 10:28:10 -0500

    purge iiab

--- box/generic/upgrade-os.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-# make sure the base operating system is updated -- the base image may be stale
-apt-get -y update
-apt-get -y dist-upgrade
-apt-get -y clean
-apt-get -y autoremove
-reboot
